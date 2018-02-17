eeec6ad7f9e56dcdb37f29a875071bdf32677237
Alain Lemaire <jaxdid@gmail.com>
2018-02-17 22:15:06 +0000

    Adds book list reducer, restructures files and Standardizes JS

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import './App.css'
+import '../App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         Hello
@@ -11,4 +11,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default () => {
+  return [
+    { title: 'The Silmarillion' },
+    { title: 'The Lord of the Rings: The Fellowship of the Ring' },
+    { title: 'The Lord of the Rings: The Two Towers' },
+    { title: 'The Lord of the Rings: The Return of the King' }
+  ]
+}
